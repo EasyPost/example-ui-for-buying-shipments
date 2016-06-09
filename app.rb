48ecb393111ace0f125a149ea3e120cde68a25f2
Nruthya <nruthya@easypost.com>
2016-06-09 13:23:53 -0700

    adjust indentaiton

--- app.rb
+++ app.rb
@@ -5,58 +5,57 @@ require 'tilt/erb'
 require 'dotenv'
 require 'pry'
 require './lib/helpers'
-
 class App < Sinatra::Base
   set :show_exceptions, false
   enable :sessions
   register Sinatra::Flash
   helpers Helpers
-	
+  
   configure :development, :test do
     Dotenv.load
   end
-	
-	configure do 	
-		EasyPost.api_key = ENV['EASYPOST_API_KEY']
-    set :addr_verification, {:verify_strict => ['delivery','zip4']}
-	end
 
-	get '/' do
-		redirect '/shipment'
-	end
+  configure do
+    EasyPost.api_key = ENV['EASYPOST_API_KEY']
+    set :addr_verification, {:verify_strict => ['delivery','zip4']}
+  end
+
+  get '/' do
+    redirect '/shipment'
+  end
 
   get '/shipment' do
     erb :index
   end
 
-	post '/shipment' do
-		unless EasyPost.api_key || EasyPost.api_key != ""
+  post '/shipment' do
+    unless EasyPost.api_key || EasyPost.api_key != ""
       halt(401, erb(:index, locals: {:error_message => "Check your API Key"}))
-		end
+    end
     # part of address object not address attrbute of shipment.
     if params[:verify] == "true"
       params[:address].merge!(settings.addr_verification)
     end
 
-    begin	
-			from_addr_id = "adr_XXXXXXXXXXXXXXXXXXXXXXXXXXX70604"
+    begin
+      from_addr_id = "adr_XXXXXXXXXXXXXXXXXXXXXXXXXXX70604"
       from_address = EasyPost::Address.retrieve(from_addr_id)
-		  to_address = EasyPost::Address.create(params[:address])
-
+      to_address = EasyPost::Address.create(params[:address])
       shipment = EasyPost::Shipment.create(
         :from_address => from_address,
         :to_address => to_address,
         :parcel => params[:parcel]
-      )
-
+        )
       redirect "shipment/#{shipment.id}"
     rescue EasyPost::Error => e
       erb :index, locals: {from_address: from_address,
-                           to_address: to_address,
-                           parcel: params[:parcel],
-                           verify: "true",  exception: e}
+        to_address: to_address,
+        parcel: params[:parcel],
+        verify: "true",
+        exception: e
+      }
     end
-	end
+  end
 
   get '/shipment/:id' do
     shipment = EasyPost::Shipment.retrieve(params[:id])
@@ -64,7 +63,7 @@ class App < Sinatra::Base
     erb :rate, locals: { shipment: shipment }
   end
 
-	post '/shipment/:id/label' do
+  post '/shipment/:id/label' do
     shipment = EasyPost::Shipment.retrieve(params[:id])
     halt 404, 'Not found' unless shipment
     begin
@@ -72,15 +71,14 @@ class App < Sinatra::Base
       raise "Failed to buy label" unless shipment.postage_label
       redirect "shipment/#{shipment.id}/label"
     rescue e
-      halt 400, erb(:rate, locals: { error_message: e.message }) 
-    end  
-	end
+      halt 400, erb(:rate, locals: { error_message: e.message })
+    end
+  end
 
-	get '/shipment/:id/label' do
-		shipment = EasyPost::Shipment.retrieve(params["id"])
+  get '/shipment/:id/label' do
+    shipment = EasyPost::Shipment.retrieve(params["id"])
     halt 404, 'Not found' unless shipment
     erb :label, locals: { shipment: shipment }
-	end
-
-	run! if app_file == $0
+  end
+  run! if app_file == $0
 end
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,9 +1,9 @@
 module Helpers
-	def render_addr_verification_messages(address)
-		errors =[]
-		address[:error][:errors].each do |error|
-			errors << (error[:message])
-		end
-		erb :_verification_message, locals: {errors: errors}
-	end
+  def render_addr_verification_messages(address)
+    errors =[]
+    address[:error][:errors].each do |error|
+      errors << (error[:message])
+    end
+    erb :_verification_message, locals: {errors: errors}
+  end
 end
